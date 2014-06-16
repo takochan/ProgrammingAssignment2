007bd99d7a7633f8d2f5bdb77a465e2c632cee79
takochan <Benroscorla@hotmail.com>
2014-06-16 20:51:59 +0100

    First Pass

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This file contains funtions that help manage the inversing 
+## of matrices. They are written to enable the cacheing of the 
+## inverted matrix.
 
-## Write a short comment describing this function
-
+## This function is a wrapper for 4 other functions which help
+## cache a matrix and it's inverted partner
 makeCacheMatrix <- function(x = matrix()) {
-
+        imatrix <- NULL
+        #sets the value for the original matrix
+        set <- function(y) {
+                x <<- y
+                imatrix <<- NULL
+        }
+        #gets the value of the original matrix
+        get <- function() x
+        #sets the inverse of the matrix
+        setInverse <- function(inverseMatrix) imatrix <<- inverseMatrix
+        #gets the inverse of the matrix
+        getInverse <- function() imatrix
+        list(set = set, get = get,
+             setInverse = setInverse,
+             getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
-
+## This function returns the inverse of a matrix 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getInverse()
+        #check to see if inverse has already been supplied and if it has return the cached version
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        #these lines execute if the cache has not been found (i.e. it is executing for the first time)
+        data <- x$get()
+        m <- solve(data) %*% data
+        x$setInverse(m)
+        m
 }
